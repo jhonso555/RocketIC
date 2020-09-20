552cf8983efc9f8a1cf353853109bec2b813a8df
Marcelo Macedo <marcelo.macedo330@hotmail.com>
2020-09-20 15:46:03 -0300

    Testes sendo realizados para compensação de curvas gráficas
    Testes para diminuir o número de curvas dos eixos X e Y no gráfico.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 cap = cv2.VideoCapture('./src/Video_4.mp4')
 frames = 1
 frame_atual = 1
+k = 0
 cX, cY = [], []
 
 coordenadas = open("coords.txt", "w")
@@ -33,17 +34,57 @@ while(frames <= int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
         cv2.imshow('Res', res)
 
 
-        if frames % 4 == 0 and frames > 20:
+        if frames % 4 == 0 and frames > 19:
             for i in range(352):
                 for j in range(640):
                     valor = mask[i][j]
                     #i = y & j = x
                     if valor != 0 and frame_atual == frames and j > 200 and j < 270:
                         frase =  "frame (%d) foi detectado em: X%d e Y%d\n" % (frames, i, j)
-                        print(j)
+                        
+                        #calcular a diferença entre o ponto atual e o ponto anterior para fazer 
+                        # append num valor médio. para diminuir a disparidade no gráfico.
+                        if len(cX) > 1 and len(cY > 1):
+                            print("X", j, cX[k-1])
+                            print("Y", i, cY[k-1])
+                            if j < cX[k-1]:
+                                if j-cX[k-1] <= 10 and j-cX[k-1] > 0 :
+                                    cX.append(j-cX[k-1])
+                                    print(j-cX[k-1] + "Carai!!")
+                                else:
+                                    continue
+                            
+                            elif cX[k-1] > j:
+                                if cX[k-1]-j <= 10 and cX[k-1] > 0:
+                                    cx.append(cX[k-1])
+                                    print("Deu certo")
+                                else:
+                                    continue
+                                
+                        elif len(cY) > 1:
+                            print("Y", i, cY[k-1])
+                            if i < cY[k-1]:
+                                if i-cY[k-1] <= 5 and i-cY[k-1] > 0:
+                                    cY.append(i-cY[k-1])
+                                    print("Deu certo 2")
+                                else:
+                                    continue
+
+                            elif cY[k-1] > i:
+                                if cY[k-1] > i:
+                                    if cY[k-1] - i <= 5 and cY[k-1]-i > 0:
+                                        cY.append(cY[k-1]-i)
+                                        print("Deu certo 3")
+                                    else:
+                                        continue
+                        else:
+                            print("Não deu certo mas vamo ve")
+                            cX.append(j)
+                            cY.append(i)
+                            k += 1
+        
+                        print("Inseri no bagui", k, cX, cY, len(cX), len(cY))
                         coordenadas.write(frase)
-                        cY.append(i)
-                        cX.append(j)
                         frame_atual += 1
     
 
@@ -54,7 +95,8 @@ while(frames <= int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
         break
     frames += 1
     frame_atual = frames
-plt.plot(cY, cX)
+ 
+plt.plot(cX, cY)
 plt.xlabel('Eixo X')
 plt.ylabel('Eixo Y')
 plt.title('Teste!!')
